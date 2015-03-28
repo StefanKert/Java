1c1e3321128cec19ba1282c17ad9dc7cca99a01c
Stefan Kert <stefan.kert@inode.at>
2015-03-28 09:04:17 +0100

    Added Hello World example

---
+++ 28032015_Übungsstunde/Hello/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		at.fhooe.swe.hello.HelloWorld.main(args);
+	}
+}
---
+++ 28032015_Übungsstunde/Hello/at/fhooe/swe/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package at.fhooe.swe.hello;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, package!");
+	}
+}
