f578f4e9344aafcfdde1e1cea942136d21bb1f00
Stefan Kert <stefan.kert@inode.at>
2015-04-04 16:07:54 +0200

    Added further tests

--- UE3_Hamming_Sort/src/at/skert/swe/ue3/Startup.java
+++ UE3_Hamming_Sort/src/at/skert/swe/ue3/Startup.java
@@ -1,20 +1,14 @@
 package at.skert.swe.ue3;
 
-import java.util.ArrayList;
-
-import at.skert.swe.ue3.hamming.Hamming;
+import at.skert.swe.ue3.hamming.tests.HammingTests;
 import at.skert.swe.ue3.sorting.tests.SortTests;
 
 public class Startup {
 
 	public static void main(String[] args) {
-		long expectedSize = 10000;
-		long startTime = System.nanoTime();
-		ArrayList<Long> smootNumbers = new Hamming().calculateFixedAmountOfSmoothNumbersBy5((int)expectedSize);
-		long smootNumber = smootNumbers.get((int) (smootNumbers.size() - 1));
-		long endTime = System.nanoTime();
-		System.out.println(smootNumber + " took " + ((endTime - startTime) / 1000000000) + " s to calculate");
-
+		HammingTests hammingTests = new HammingTests();
+		hammingTests.TestGenerateSpecificHammingNumber(10000);
+		
 		SortTests sortTests = new SortTests();
 		try {
 			sortTests.AssertQuickSortArrayIsSorted();
--- UE3_Hamming_Sort/src/at/skert/swe/ue3/hamming/Hamming.java
+++ UE3_Hamming_Sort/src/at/skert/swe/ue3/hamming/Hamming.java
@@ -7,9 +7,6 @@ public class Hamming {
 	final static long two = 2;
 	final static long three = 3;
 	final static long five = 5;
-	private long x2 = two;
-	private long x3 = three;
-	private long x5 = five;
 	private ArrayList<Long> hammingNumbers;
 	
 	interface IHammingBreakCondition {
@@ -20,16 +17,21 @@ public class Hamming {
 	}
 	
 	public ArrayList<Long> calculateSmoothNumbersBy5UntilBoundary(int boundary){
-		return calculateSmoothNumbersBy5(nextValue -> nextValue > boundary);
+		return calculateSmoothNumbersBy5(currentValue -> currentValue > boundary);
 	}
-	public ArrayList<Long> calculateFixedAmountOfSmoothNumbersBy5(int n){
-		return calculateSmoothNumbersBy5(nextValue -> hammingNumbers.size() > n);
+	public ArrayList<Long> calculateFixedAmountOfSmoothNumbersBy5(int amount){
+		return calculateSmoothNumbersBy5(nextValue -> hammingNumbers.size() >= amount);
+	}
+	public long calculateSmoothNumberBy5(int n){
+		ArrayList<Long> list = calculateFixedAmountOfSmoothNumbersBy5(n);
+		return list.get(list.size() - 1);
 	}
 	
 	private ArrayList<Long> calculateSmoothNumbersBy5(IHammingBreakCondition breakCondition) {
-
+		hammingNumbers.clear();
 		hammingNumbers.add((long) 1);
 		int i = 0, j = 0, k = 0;
+		long x2 = two, x3 = three, x5 = five;
 		int index = 1;
 		while(true){
 			long currentValue = Math.min(x2, Math.min(x3, x5));
@@ -37,27 +39,18 @@ public class Hamming {
 				break;
 			}
 			hammingNumbers.add(currentValue);
-			i = SetX2IfNeeded(index, i);
-			j = SetX3IfNeeded(index, j);
-			k = SetX5IfNeeded(index, k);
+			if (hammingNumbers.get(index) == x2)
+				x2 = recalculateValue(two, ++i);
+			if (hammingNumbers.get(index) == x3)
+				x3 = recalculateValue(three, ++j);
+			if (hammingNumbers.get(index) == x5)
+				x5 = recalculateValue(five, ++k);
 			index++;
 		}
 		return hammingNumbers;
 	}
-	private int SetX2IfNeeded(int index,  int i){
-		if (hammingNumbers.get(index) == x2)
-			x2 = two * hammingNumbers.get(++i);
-		return i;
+	
+	private long recalculateValue(long fixedValue, int listIndex){
+		return fixedValue * hammingNumbers.get(listIndex);
 	}
-	private int SetX3IfNeeded(int index,  int j){
-		if (hammingNumbers.get(index) == x3)
-			x3 = three * hammingNumbers.get(++j);
-		return j;
-	}
-	private int SetX5IfNeeded(int index,  int k){
-		if (hammingNumbers.get(index) == x5)
-			x5 = five * hammingNumbers.get(++k);
-		return k;
-	}
-
 }
